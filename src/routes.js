d177113081af07bfb94a03f99828afea6b85de07
Fernado Lima <fernandovlima.s@gmail.com>
2020-02-14 11:30:47 -0300

    jwt token in session

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
+
+// session routes
+routes.post('/sessions', SessionController.store);
 
 // user routes
 routes.post('/users', UserController.store);
