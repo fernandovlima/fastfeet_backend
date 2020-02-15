9f67d3a1bfe271247905afecc94956d64014ffc9
Fernado Lima <fernandovlima.s@gmail.com>
2020-02-15 11:22:24 -0300

    file upload controller

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,11 @@ import Sequelize from 'sequelize';
 // models
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient];
+const models = [User, Recipient, File];
 
 class Database {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 // middleware
 import AuthMiddleware from './app/middlewares/auth';
@@ -27,8 +28,6 @@ routes.put('/users', UserController.update);
 // create recipient
 routes.post('/recipients', RecipientController.store);
 // upload files
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
